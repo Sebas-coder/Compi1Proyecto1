e8580867f2e8b8ec0dfda4a154595297689064dc
Sebas-coder <jairosebastianrp@gmail.com>
2020-09-10 00:03:45 -0600

    Finalizacion de analisis sintactico

--- RmtAnalizer/ScannerRmt.py
+++ RmtAnalizer/ScannerRmt.py
@@ -14,25 +14,79 @@ class Scanner:
     def inicio(self,lista):
         self.lista_tokens = lista
         self.lista_size = len(self.lista_tokens) - 1
+        self.pre_analisis = self.lista_tokens[self.indice]
         
+    def E(self):
+        self.T() 
+        self.EP()
+        
+    def EP(self):
+        # +
+        if self.pre_analisis == TT.MAS:
+            self.parea(TT.MAS)
+            self.T()
+            self.EP()
+        # |*
+        elif self.pre_analisis == TT.MENOS:
+            self.parea(TT.MENOS)
+            self.T()
+            self.EP()
+        # |epcilon
+        else:
+            return
     
+    def T(self):
+        self.F()
+        self.TP()
+        
+    def TP(self):
+        # *
+        if self.pre_analisis == TT.ASTERISCO:
+            self.parea(TT.ASTERISCO)
+            self.F()
+            self.TP()
+        # |/
+        elif self.pre_analisis == TT.DIAGONAL:
+            self.parea(TT.DIAGONAL)
+            self.F()
+            self.TP()
+        # |epcilon
+        else:
+            return
+    
+    def F(self):
+        # Numero
+        if self.pre_analisis == TT.NUMERO:
+            self.parea(TT.NUMERO)
+        # |ID
+        elif self.pre_analisis == TT.ID:
+            self.parea(TT.ID)
+        # |(
+        elif self.pre_analisis == TT.PARENIZQ:
+            self.parea(TT.PARENIZQ)
+            self.E()
+            self.parea(TT.PARENDER)
     
     def parea(self, tipoToken):
         # Si esta incorrecto
         if self.bandera:
+            # Solo itera hasta la ultima posicion
             if self.indice < self.lista_size:
                 self.indice += 1
                 self.pre_analisis = self.lista_tokens[self.indice]
-            else:
-                print("Llego al token final")
         # Si correcto
         else: 
             # Coincide el token
             if self.pre_analisis == tipoToken:
+                # Coincide y pasa al siguiente
                 if self.indice < self.lista_size:
                     self.indice += 1
                     self.pre_analisis = self.lista_tokens[self.indice]
             # No coincide error
             else: 
-                #Se detecta error
+                #Se detecta error y pasa al siguiente
                 self.bandera = True
+                if self.indice < self.lista_size:
+                    self.indice += 1
+                    self.pre_analisis = self.lista_tokens[self.indice]
+        
